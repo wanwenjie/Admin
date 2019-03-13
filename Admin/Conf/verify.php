3e4f87c51a0234fc35d328e8f2901e8bec1bdb2a
wanwenjie <15263095729@163.com>
2019-03-13 12:59:49 +0800

    Update verify.php

--- Admin/Conf/verify.php
+++ Admin/Conf/verify.php
@@ -5,7 +5,7 @@
 'M_Domain'	=>	'thinkphp',
 'M_Keywords'	=>	'最新，电影，院线',
 'M_Content'	=>	'提供最新电影观看',
-'M_Email'	=>	'1654206898@qq.com',
+'M_Email'	=>	'****@qq.com',
 'M_Theme'	=>	'Default',
 'M_visit'	=>	'',
 'M_ad'		=>	'',
