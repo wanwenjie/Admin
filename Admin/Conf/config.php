e17308d145588437009ddad3304aa8a8b7082aa8
wanwenjie <15263095729@163.com>
2019-03-13 12:59:17 +0800

    Update config.php

--- Admin/Conf/config.php
+++ Admin/Conf/config.php
@@ -6,7 +6,7 @@ return array(
     'DB_HOST'               =>  'localhost', // 服务器地址
     'DB_NAME'               =>  'admin',          // 数据库名
     'DB_USER'               =>  'root',      // 用户名
-    'DB_PWD'                =>  'usbw',          // 密码
+    'DB_PWD'                =>  '****',          // 密码
     'DB_PORT'               =>  '3306',        // 端口
     'DB_PREFIX'             =>  'mac_',    // 数据库表前缀
     'LOAD_EXT_CONFIG'		=>	'verify',
